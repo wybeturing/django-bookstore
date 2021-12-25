15316799b51b94f38400ca512f5f3b9505fea559
Ndze'dzenyuy Lemfon K.V <karllemfon@gmail.com>
2021-12-25 21:15:22 +0000

    Added reviews to the books app, so now when we click on the details of a book we can see the reviews as well. The only problem now is the fact that we can only add reviews from the admin's end of the app

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Review
+
+class ReviewInLine(admin.TabularInline):
+    model = Review 
 
 class BookAdmin(admin.ModelAdmin):
+    inlines = [
+        ReviewInLine,
+    ]
     list_display = ('title', 'author', 'price')
 
 # Register your models here.
