15cdee7f0c4a543dcd5627ce384d2d218e95d9fe
Ndze'dzenyuy Lemfon K.V <karllemfon@gmail.com>
2021-12-25 09:19:16 +0000

    Added some styling using Bootstrap. Key thing is load the bootstrap CDN before your custome css styles in the static folder, and close to the end of the file, load jQuery first, then Popper, then the JS paths, and then load your own JS paths

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
