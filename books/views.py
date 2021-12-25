dc56ba992c5aa4b76b3f87dabfac6f26a056ebc2
Ndze'dzenyuy Lemfon K.V <karllemfon@gmail.com>
2021-12-25 22:09:24 +0000

    Restricted access to the books page only to persons who are currently logged in

--- books/views.py
+++ books/views.py
@@ -1,14 +1,17 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from .models import Book
 from django.views.generic import ListView, DetailView 
 
 # Create your views here.
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
+    login_url = 'account_login'
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, DetailView):
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
+    login_url = 'account_login'
